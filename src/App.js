d211fa1409c84f1e92e534fc26f7541c48004033
Evgeny Strelnikov <estrelnikov1@yandex.ru>
2023-03-12 23:34:19 +0300

    localStorage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Fetch } from "./components/fetch";
+import { LocalStorage } from "./components/localStorage";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <p>Custom Hooks Tasks</p>
         <Fetch />
+        <LocalStorage />
       </header>
     </div>
   );
